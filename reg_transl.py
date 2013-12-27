04ac4f599ba0b0f7d8c57dd05c391a82fab0e1a3
Shawn Oakley <shawnoakley@gmail.com>
2013-12-26 20:23:49 -0500

    Correctly accesses registry file on current user.  That file appears to be empty, however.  Need to consider Windows 7 registry structure more carefully.

--- reg_transl.py
+++ reg_transl.py
@@ -16,35 +16,41 @@ def val2addr(val):
 import sys
 # from Registry import Registry
 
-import winreg_unicode as winreg
+from winreg import *
 # _winreg only useful running on PC natively.  Will throw error in non-Windows environment,
 
 # Prints all keys in a Registry
 
 def rec(key, depth=0):
-	print "\t" * depth + key.path()
+	print("\t" * depth + key.path())
 	for subkey in key.subkeys():
 		rec(subkey, depth + 1)
 
 def printNets(reg = ""):
 	net = "Network"
-	with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, net) as key:
-		print "\n[*] Networks You have Joined."
+	# net = "SOFTWARE"
+	# NOTE: The below method call appears specific to Windows-7.  
+	# The 'network' subkey in localhost doesn't exist, per se.  It merely references the subkey as
+	# it exists in the user.
+	# http://serverfault.com/questions/522065/missing-hkcu-network-subkey-when-browsing-registry-remotely-with-powershell
+	# Below code does not print anything as that registry file appears to be empty, along with
+	# the other network reg files to which I have access.
+	with OpenKey(HKEY_CURRENT_USER, net) as key:
+		print("\n[*] Networks You have Joined.")
 		for i in range(100):
 			try:
-				guid = winreg.EnumKey(key, i)
-				with winreg.OpenKey(key, str(guid)) as netKey:
-					(n, adrr, t) = winreg.EnumValue(netKey, 5)
-					(n, name, t) = winreg.EnumValue(netKey, 4)
+				guid = EnumKey(key, i)
+				with OpenKey(key, str(guid)) as netKey:
+					(n, adrr, t) = EnumValue(netKey, 5)
+					(n, name, t) = EnumValue(netKey, 4)
 					macAddr = val2addr(addr)
 					netName = str(name)
-					print '[+]' + netName + ' ' + macAddr
+					print('[+]' + netName + ' ' + macAddr)
 			except:
-				break
-	# reg = Registry.Registry(reg || "SOFTWARE\Microsoft\Windows NT\Current Version"+\
-	# "\NetworkList\Signatures]Unmanaged")
-
-	rec(reg.root())
+				print("Something happened")
+				# break
+	# reg = Registry.Registry(reg or "SOFTWARE\\Microsoft\Windows NT\\Current Version\\NetworkList\\Signatures\\Unmanaged")
+	# rec(reg.root())
 
 
 
@@ -63,5 +69,6 @@ def printNets(reg = ""):
 
 def main():
 	printNets()
+
 if __name__ == "__main__":
 	main()
